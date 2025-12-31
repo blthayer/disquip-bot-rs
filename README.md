cedb88edb5a0ff09a11a2ec0663fd3cff6befc30
Brandon Thayer <blt@tuta.com>
2025-12-31 10:42:43 -0800

    Initial hello world messaging

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # disquip-bot-rs
 Re-implementation of the now defunct [disquip-bot](https://github.com/blthayer/disquip-bot) in Rust
+
+## Setup
+
+This assumes an "App" in Discord parlance has already been created. Creating an
+app/bot is outside the scope of this document.
+
+1. Log into the Discord developer portal.
+2. Under the "Bot" tab, click "Reset Token."
+3. Copy the contents of the token into a file called `token`
+4. Back in the developer portal under the "Bot" tab, in the "Privileged Gateway
+   Intents" sections toggle "Message Content Intent" on.
+5. Save changes.
+
+## Usage
+
+1. Build: `cargo build --release`
+2. Run: `DISCORD_TOKEN="$(<token)" ./target/release/disquip-bot-rs`
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use poise::serenity_prelude as serenity;
+
+struct Data {} // User data, which is stored and accessible in all command invocations
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+/// Responds "world" to "hello"
+#[poise::command(prefix_command)]
+async fn hello(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.say("world").await?;
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    let token = std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN");
+
+    let intents = serenity::GatewayIntents::non_privileged()
+        | serenity::GatewayIntents::GUILD_MESSAGES
+        | serenity::GatewayIntents::MESSAGE_CONTENT;
+
+    // let intents = serenity::GatewayIntents::non_privileged();
+    let prefix_framework_options = poise::PrefixFrameworkOptions {
+        prefix: Some("!".to_string()),
+        ..Default::default()
+    };
+
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            prefix_options: prefix_framework_options,
+            commands: vec![hello()],
+            ..Default::default()
+        })
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        })
+        .build();
+
+    let client = serenity::ClientBuilder::new(token, intents)
+        .framework(framework)
+        .await;
+    client.unwrap().start().await.unwrap();
 }
