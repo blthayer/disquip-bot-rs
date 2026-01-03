9467d99cf534fa895ae5e5d34ffd4f92a9eb58fa
Brandon Thayer <blt@tuta.com>
2026-01-02 16:56:51 -0800

    Clean up hwlp

--- src/main.rs
+++ src/main.rs
@@ -85,7 +85,11 @@ pub async fn help(
     #[description = "Specific command to show help about"] command: Option<String>,
 ) -> Result<(), Error> {
     let config = poise::builtins::HelpConfiguration {
-        extra_text_at_bottom: "Type \"!help command\" for more info on a command. \"!list\" is your friend for discovering quips.",
+        extra_text_at_bottom: "\
+Type \"!category number\" (e.g., \"aoe1 1\") to play a quip!
+Type \"!list\" to discover available quip categories.
+Type \"!list category\" to get available quip numbers for the given category.
+Type \"!help command\" for more info on a command.",
         ..Default::default()
     };
     poise::builtins::help(ctx, command.as_deref(), config).await?;
