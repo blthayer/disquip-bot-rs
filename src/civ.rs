9f1eed7305add1aa4c904a766fa3a58b46442315
Brandon Thayer <blt@tuta.com>
2026-01-31 15:39:59 -0800

    Working code for drawing n random leaders.

--- src/civ.rs
+++ src/civ.rs
@@ -1,4 +1,7 @@
+use rand::rng;
+use rand::seq::SliceRandom;
 use serde::Deserialize;
+use std::collections::HashSet;
 use std::fs::File;
 
 #[derive(Debug, Deserialize, Eq, PartialEq)]
@@ -23,6 +26,35 @@ fn read_leaders() -> Vec<Leader> {
     leaders
 }
 
+fn draw_leaders(n: usize) -> Vec<Leader> {
+    // Could take leaders as input, but there's really no reason to...
+    let mut all_leaders = read_leaders();
+    // Since we're forbidding duplicate leaders and civs we cannot just
+    // draw n_players * n_leaders
+    all_leaders.shuffle(&mut rng());
+    let mut out: Vec<Leader> = Vec::with_capacity(n);
+    let mut civs: HashSet<String> = HashSet::new();
+    let mut names: HashSet<String> = HashSet::new();
+
+    // A while loop feels more natural, but Rust's ownership
+    // model essentially forces a for loop here.
+    for leader in all_leaders {
+        if !civs.contains(&leader.civ) && !names.contains(&leader.name) {
+            // Feels bad to clone here, but I'm immediately seeing an
+            // obvious way around it.
+            civs.insert(leader.civ.clone());
+            names.insert(leader.name.clone());
+            out.push(leader);
+        }
+
+        if out.len() == n {
+            break;
+        }
+    }
+    // Technically, we should raise an error or something if out.len() < n.
+    // But... let's keep it simple instead.
+    out
+}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -35,5 +67,13 @@ mod tests {
             civ: "French".to_string(),
         };
         assert_eq!(catherine, results[6]);
+    }
+
+    #[test]
+    fn test_draw_leaders() {
+        for n in 1..50 {
+            let leaders = draw_leaders(n);
+            assert_eq!(leaders.len(), n);
+        }
     }
 }
